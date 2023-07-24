7b9f4b16a9d1f91df70165adacac6bc6f22f303a
lillianlaihughes <lillianlaihughes@gmail.com>
2023-07-24 17:37:16 -0400

    updated js and added image assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
-import BucketList from './components/BucketList';
+import React, { useState } from 'react';
+import Header from './components/header';
+import Navigation from './components/navigation';
+import AboutMe from './components/aboutMe';
+import Portfolio from './components/portfolio';
+import Contact from './components/contact';
+import Resume from './components/resume';
+import Footer from './components/footer';
+import './index.css';
 
-function App() {
+
+const App = () => {
+
+  const [selectedSection, setSelectedSection] = useState('About Me');
+
+
   return (
-    <div className="bucket-app">
-      <BucketList />
+    <div className="App">
+      <Header />
+      <Navigation selectedSection={selectedSection} setSelectedSection={setSelectedSection} />
+      <main className='flex'>
+        {selectedSection === 'About Me' && <AboutMe />}
+        {selectedSection === 'Portfolio' && <Portfolio />}
+        {selectedSection === 'Contact' && <Contact />}
+        {selectedSection === 'Resume' && <Resume />}
+      </main>
+      <Footer />
     </div>
   );
 }
