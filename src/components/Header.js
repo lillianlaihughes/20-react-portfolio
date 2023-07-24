8ff74fb5e9e6020807ca34163ebeeea7bfaa3389
lillianlaihughes <lillianlaihughes@gmail.com>
2023-07-24 19:46:45 -0400

    change to header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import headShot from '../assets/Lillian-other-headshot.jpg';
 const Header = () => {
   return (
     <header class='flex flex-row bg-gradient-to-r from-cyan-200 to-pink-200 justify-center'>
-      <img className='w-64 rounded-full' src={headShot} alt='Lillian' />
+      <img className='w-4 rounded-full' src={headShot} alt='Lillian' />
       <p>Lillian may or may not know what she is doing, tbh.</p>
     </header>
   );
